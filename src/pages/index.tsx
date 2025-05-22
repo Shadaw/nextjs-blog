4e588f212c52039b68308942ff3d4d6c3f2866ac
Gustavo Alexandre <gustavoalprofissional@gmail.com>
2025-05-22 14:53:10 -0300

    feat: header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
+import { Geist, Geist_Mono } from 'next/font/google';
+
+import { Header } from '@/components/header';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen min-w-full bg-slate-900">
-      <h2 className="text-4xl text-stone-50 font-bold">Hello world</h2>
+    <div
+      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+    >
+      <Header />
+
     </div>
   );
 }
