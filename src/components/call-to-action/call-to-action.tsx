8ebff486eb21cd45845ef192c8330333cc9ccb58
Gustavo Alexandre <gustavoalprofissional@gmail.com>
2025-07-02 16:47:36 -0300

    feat: adding missing images

--- src/components/call-to-action/call-to-action.tsx
+++ src/components/call-to-action/call-to-action.tsx
@@ -10,8 +10,10 @@ const ptSansCaption = PT_Sans_Caption({
 
 export function CallToAction() {
   return (
-    <section className="py-24 bg-gradient-to-b from-cyan-950/20 to-gray-700">
-      <div className="container flex flex-col items-center">
+    <section className="relative py-24 bg-gradient-to-b from-cyan-950/20 to-gray-700">
+      <div className="absolute inset-0 bg-[url('/background-footer.svg')] bg-cover bg-center bg-no-repeat opacity-70"/>
+
+      <div className="relative container flex flex-col items-center">
         <div className="p-4 bg-cyan-300 w-fit rounded-full absolute mt-[-120px] ">
           <Store className="text-cyan-100" />
         </div>
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -24,7 +24,7 @@ export function Layout({ children }: LayoutProps) {
     <div className={`${inter.className} ${ptSansCaption.className} relative flex min-h-screen flex-col bg-gray-700 font-inter`}>
       <Header />
 
-      <main className="flex-1 flex flex-col mb-12 mt-10">
+      <main className="flex-1 flex flex-col mt-10">
         {children}
       </main>
 
