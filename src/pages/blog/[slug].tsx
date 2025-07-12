d9891ff7bccdad3e0f8dde02d496596da882f178
Gustavo Alexandre <gustavoalprofissional@gmail.com>
2025-07-12 20:59:12 -0300

    feat: create Markdown component

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -11,6 +11,7 @@ import {
   BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb'
 import { Avatar } from '@/components/avatar'
+import { Markdown } from '@/components/markdown'
 
 export default function PostPage() {
   const router = useRouter()
@@ -22,47 +23,59 @@ export default function PostPage() {
 
   return (
     <main className="mt-32 text-gray-100">
-      <Breadcrumb>
-        <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbLink asChild className="text-action-sm">
-              <Link href="/blog">Blog</Link>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <span className="text-action-sm text-blue-200">{post?.title}</span>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
+      <div className="container space-y-12 px-4 md:px-8">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild className="text-action-sm">
+                <Link href="/blog">Blog</Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <span className="text-action-sm text-blue-200">
+                {post?.title}
+              </span>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
 
-      <div className="grid grid-cols-1 gap-6 lg:grid-cols-[1fr_300px] lg:gap-12">
-        <article className="overflow-hidden rounded-lg border-[1px] border-gray-400 bg-gray-600">
-          <figure className="relative aspect-[16/10] w-full overflow-hidden rounded-lg">
-            <Image
-              src={post?.image}
-              alt={post?.title}
-              fill
-              className="object-cover"
-            />
-          </figure>
+        <div className="grid grid-cols-1 gap-6 lg:grid-cols-[1fr_300px] lg:gap-12">
+          <article className="overflow-hidden rounded-lg border-[1px] border-gray-400 bg-gray-600">
+            <figure className="relative aspect-[16/10] w-full overflow-hidden rounded-lg">
+              <Image
+                src={post?.image}
+                alt={post?.title}
+                fill
+                className="object-cover"
+              />
+            </figure>
 
-          <header className="p-4 pb-0 md:p-6 lg:p-12">
-            <h1 className="mb-6 text-balance text-heading-lg md:text-heading-xl lg:text-heading-xl">
-              {post?.title}
-            </h1>
+            <header className="mt-8 p-4 pb-0 md:mt-12 md:p-6 lg:p-12 lg:pb-6">
+              <h1 className="mb-8 text-balance text-heading-lg md:text-heading-xl lg:text-heading-xl">
+                {post?.title}
+              </h1>
 
-            <Avatar.Container>
-              <Avatar.Image src={post?.author.avatar} alt={post?.author.name} />
-              <Avatar.Content>
-                <Avatar.Title>{post?.author.name}</Avatar.Title>
-                <Avatar.Description>
-                  Publicado em <time dateTime={post.date}>{publishedDate}</time>
-                </Avatar.Description>
-              </Avatar.Content>
-            </Avatar.Container>
-          </header>
-        </article>
+              <Avatar.Container>
+                <Avatar.Image
+                  src={post?.author.avatar}
+                  alt={post?.author.name}
+                />
+                <Avatar.Content>
+                  <Avatar.Title>{post?.author.name}</Avatar.Title>
+                  <Avatar.Description>
+                    Publicado em{' '}
+                    <time dateTime={post.date}>{publishedDate}</time>
+                  </Avatar.Description>
+                </Avatar.Content>
+              </Avatar.Container>
+            </header>
+
+            <div className="prose prose-invert mt-12 max-w-none px-4 md:px-6 lg:px-12">
+              <Markdown content={post.body.raw} />
+            </div>
+          </article>
+        </div>
       </div>
     </main>
   )
